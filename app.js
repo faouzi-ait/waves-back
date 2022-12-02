31ca46a810d3140b36d7cb0126520519c0e6d996
Faouzi Aitelhara <faouzi.aitelhara@gmail.com>
2022-12-02 17:12:14 +0000

    Fixed OAuth access

--- app.js
+++ app.js
@@ -15,7 +15,21 @@ app.use(
 );
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({ origin: '*' }));
+app.use(function (req, res, next) {
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type'
+  );
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('set-cookie', ['sameSite=none; Secure'], true);
+  next();
+});
 
 app.use("/api/v1", require("./routes/UserRoutes"));
 app.use("/api/v1", require("./routes/ProductsRoutes"));
--- configuration/oauth.js
+++ configuration/oauth.js
@@ -1,7 +1,10 @@
 module.exports = {
-    type: 'oauth2',
-    user: process.env.user,
-    clientId: process.env.clientId,
-    clientSecret: process.env.clientSecret,
-    refreshToken: '1//0473R-aoau6JSCgYIARAAGAQSNwF-L9IrWQn90L6gAY_pyWEg7OX2VNI7kX3rqAA4APmZPH1I1rh7kx7bNn3H-6Kuqqu0dnmEKx0'
-}
+  type: 'oauth2',
+  user: process.env.user,
+  clientId: process.env.clientId,
+  clientSecret: process.env.clientSecret,
+  refreshToken:
+    '1//04gJ23bTzZf7qCgYIARAAGAQSNwF-L9Irm6zITSXgogdAiDeblzLx80ORxE0cklHBi7CfZg0RVaDTqbaxiMfb4ljJryYrqDy2KXA',
+  accessToken:
+    'ya29.a0AeTM1idPcMjFVPZtCYBJsB_q3G6Vte9vcL1IHHRUSV7PIrolx38nOt0T9NnOw7_x96vLK32TTj9HThTBKBdcp9VNUZgs6ewF1-ay7-IfpIz8afZ3LpFB4H6O45WwmOSC3GA6JYhE3uILseq0powLAak5BS3qaCgYKAWESARISFQHWtWOmjWlfp2I15K_COdvbEQhNcw0163',
+};
