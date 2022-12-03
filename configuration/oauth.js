001598dc47d5b87a35ae5f0e0f078c57ac934322
Faouzi Aitelhara <faouzi.aitelhara@gmail.com>
2022-12-03 16:05:59 +0000

    Fixed message access

--- configuration/oauth.js
+++ configuration/oauth.js
@@ -4,7 +4,5 @@ module.exports = {
   clientId: process.env.clientId,
   clientSecret: process.env.clientSecret,
   refreshToken:
-    '1//048zwu9afJz5eCgYIARAAGAQSNwF-L9Ir1iJn6_zW1q5HkQrlxYUvYp_N7-yfo6_OetY-cGxCJjsb265Rqzl_R9E3sQWN6RVwWhk',
-  accessToken:
-    'ya29.a0AeTM1ifDQ86RXtXiNsTTAnlBFXyQ0kEFkIB_V3ajBUTRAwO5IYoeBZOcoCOINwXGpITkXjAqjfraT5CLyjiD4JvrXlUAMSEzmcEon2Ungs850kBZ5pVWbv8JWeFHDtWrVO0edHHRSqgzVjnNIhQl1v6ylmgkaCgYKAR0SARISFQHWtWOmzRy_GyTg3nQwnS7gXuLz7g0163',
+    '1//04xcRjqkG35BoCgYIARAAGAQSNwF-L9IrG0TduXXxipgUvyrGtz6b_uytKtpTcGFHmyeffS7gz9e7PEu1YeIGSl7FNeT_so58FcA',
 };
